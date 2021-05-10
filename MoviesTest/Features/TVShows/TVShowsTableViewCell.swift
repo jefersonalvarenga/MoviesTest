e013319d3a67cb6f3f26995cccf40982f52ae57e
Jeferson Alvarenga <jeferson.alvarenga@me.com>
2021-05-10 11:28:41 -0300

    Adding the feature MVVM code

---
+++ MoviesTest/Features/TVShows/TVShowsTableViewCell.swift
@@ -0,0 +1,102 @@
+//
+//  MoviewCollectionViewCell.swift
+//  MoviesTest
+//
+//  Created by Jeferson Alvarenga on 08/05/21.
+//  Copyright © 2021 Jeferson Alvarenga. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Alamofire
+import AlamofireImage
+
+class TVShowsTableViewCell: UITableViewCell {
+        
+    lazy var imgMovie: UIImageView = {
+        let img = UIImageView()
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds = true
+        return img
+    }()
+    
+    lazy var titleBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    lazy var lblTitle: UILabel = {
+        let lbl = UILabel()
+        lbl.font = .boldSystemFont(ofSize: 30)
+        lbl.textColor = .black
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+    
+    lazy var actView: UIActivityIndicatorView = {
+        let act = UIActivityIndicatorView()
+        act.hidesWhenStopped = true
+        act.startAnimating()
+        return act
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .black
+        viewCodeSetup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imgMovie.af.cancelImageRequest()
+        imgMovie.layer.removeAllAnimations()
+        imgMovie.image = nil
+    }
+    
+    func loadPoster(movie: Movie) {
+        guard let path = movie.image?.original,
+            let url = URL(string: path) else { return }
+        lblTitle.text = movie.name
+        imgMovie.af.setImage(withURL: url, cacheKey: path, imageTransition: UIImageView.ImageTransition.crossDissolve(0.5)) { (response) in
+            self.actView.stopAnimating()
+        }
+    }
+}
+
+extension TVShowsTableViewCell: ViewCodeProtocol {
+    
+    func viewAddElements() {
+        contentView.addSubview(imgMovie)
+        contentView.addSubview(titleBackground)
+        contentView.addSubview(lblTitle)
+        contentView.addSubview(actView)
+    }
+    
+    func viewConstraintElements() {
+        
+        imgMovie.snp.makeConstraints { (mkr) in
+            mkr.top.left.right.equalToSuperview()
+            mkr.bottom.equalTo(titleBackground.snp.top)
+        }
+        
+        titleBackground.snp.makeConstraints { (mkr) in
+            mkr.bottom.equalToSuperview().inset(20)
+            mkr.width.equalToSuperview()
+            mkr.height.equalTo(70)
+        }
+        
+        lblTitle.snp.makeConstraints { (mkr) in
+            mkr.left.right.equalToSuperview().inset(20)
+            mkr.centerY.equalTo(titleBackground)
+        }
+        
+        actView.snp.makeConstraints { (mkr) in
+            mkr.center.equalToSuperview()
+        }
+    }
+}
---
+++ MoviesTest/Features/TVShows/TVShowsViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  HomeInteractor.swift
+//  MoviesTest
+//
+//  Created by Jeferson Alvarenga on 08/05/21.
+//  Copyright © 2021 Jeferson Alvarenga. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+enum HomeViewState {
+    case initial
+    case loading
+    case dataLoaded
+    case newDataLoaded([IndexPath])
+    case error(Error)
+}
+
+class TVShowsViewModel {
+    
+    var viewState: BehaviorSubject<HomeViewState> = BehaviorSubject(value: .initial)
+    var movies: [Movie] = []
+    var searchMovies: [Movie] = []
+    private var backupMovies: [Movie] = []
+    private var nextPage = 0
+    private var isFetchInProgress = false
+    private var isSearch = false
+    
+    var source: [Movie] {
+        if isSearch {
+            return searchMovies
+        } else {
+            return movies
+        }
+    }
+    
+    func viewWillAppear() {
+        loadMovies()
+    }
+    
+    func loadMovies() {
+        guard !isFetchInProgress && !isSearch else {
+          return
+        }
+        viewState.onNext(.loading)
+        isFetchInProgress = true
+        let networkService = TVShowsNetwortService()
+        networkService.loadMovies(page: nextPage, onSuccess: { (movies) in
+            self.isFetchInProgress = false
+            self.nextPage += 1
+            if self.movies.count == 0 {
+                self.movies.append(contentsOf: movies)
+                self.viewState.onNext(.dataLoaded)
+            } else {
+                self.movies.append(contentsOf: movies)
+                let indexPathsToReload = self.getNextIndexPaths(from: movies)
+                self.viewState.onNext(.newDataLoaded(indexPathsToReload))
+            }
+        }, onFailure:  { (error) in
+            self.isFetchInProgress = false
+            self.viewState.onNext(.error(error))
+        })
+    }
+    
+    func search(term: String) {
+        guard !isFetchInProgress else {
+          return
+        }
+        if term.isEmpty {
+            isSearch = false
+            viewState.onNext(.dataLoaded)
+            return
+        }
+        isSearch = true
+        viewState.onNext(.loading)
+        isFetchInProgress = true
+        let networkService = TVShowsNetwortService()
+        networkService.searchMovies(term: term, onSuccess: { (movies) in
+            self.isFetchInProgress = false
+            self.searchMovies = movies.map({ (item) -> Movie in
+                self.isFetchInProgress = false
+                return Movie(name:
+                                item.show.name,
+                                image: item.show.image,
+                                summary: item.show.summary,
+                                score: item.score)
+            })
+            self.viewState.onNext(.dataLoaded)
+        }, onFailure: {(error) in
+            self.isFetchInProgress = false
+            print(error)
+        })
+    }
+    
+    func getNextIndexPaths(from tvShows: [Movie]) -> [IndexPath] {
+      let startIndex = movies.count - tvShows.count
+      let endIndex = startIndex + tvShows.count
+      return (startIndex..<endIndex).map { IndexPath(row: $0, section: 0) }
+    }
+    
+    func filter(term: String) {
+        if !isSearch {
+            isSearch = true
+            backupMovies = movies
+        } else {
+            if term.isEmpty {
+                isSearch = false
+                movies = backupMovies
+            } else {
+                movies = movies.filter{$0.name.localizedCaseInsensitiveContains(term)}
+            }
+            viewState.onNext(.dataLoaded)
+        }
+    }
+}
